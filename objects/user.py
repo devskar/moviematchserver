133a8073119fdacef77e617a2c47bdc484f6ecb4
oskar <ooskar@gmx.de>
2021-03-10 23:38:31 +0100

    adding __eq__ and __hash__

--- objects/user.py
+++ objects/user.py
@@ -3,3 +3,11 @@ class User:
         self.sid = sid
         self.name = name
 
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.sid == other.sid
+        else:
+            return NotImplemented
+
+    def __hash__(self):
+        return hash(self.sid)
