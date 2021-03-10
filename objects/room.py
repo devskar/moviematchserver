b25af6319e8953e4a0eb8caab7e77fbb072cbe5b
oskar <ooskar@gmx.de>
2021-03-10 23:37:37 +0100

    adding votes room close if empty

--- objects/room.py
+++ objects/room.py
@@ -1,5 +1,4 @@
 from objects.user import User
-from manager.moviemanager import Genre, Language
 
 
 class Room:
@@ -13,14 +12,26 @@ class Room:
 
         self.participants = set()
 
-        self.genre = Genre.ALL
-        self.language = Language.EN
-
         self.current_movie = mm.get_random_movie()
 
-    def change_genre(self, genre):
-        """Changes the current genre of the room"""
-        self.genre = genre
+        self.positive_votes = set()
+        self.negative_votes = set()
+
+    def positive_vote_movie(self, user):
+        self.positive_votes.add(user)
+
+    def negative_vote_movie(self, user):
+        self.negative_votes.add(user)
+
+    def reset_votes(self):
+        self.positive_votes.clear()
+        self.negative_votes.clear()
+
+    def everyone_voted(self):
+        return self.get_vote_count() == len(self.participants)
+
+    def get_vote_count(self):
+        return len(self.positive_votes.union(self.negative_votes))
 
     def add_user(self, user: User):
         """Adds a user to the room. first one will be the admin of the room"""
@@ -33,8 +44,15 @@ class Room:
         """Remove a user from the room"""
         self.participants.remove(user)
 
+        if user in self.negative_votes:
+            self.negative_votes.remove(user)
+        if user in self.positive_votes:
+            self.positive_votes.remove(user)
+
+        # if no users are in the room the room closes
+        if len(self.participants) == 0:
+            self.close()
+
     def close(self):
         """Close the room"""
         del self
-
-
